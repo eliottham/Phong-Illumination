d9b05fc25332b8b7ea2c8e30b4751d252b12b590
Eliott Ham <eliotthham@gmail.com>
2018-02-11 20:34:04 -0800

    Initial Commit

---
+++ CSE167 Modern OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE167 Modern OpenGL
+//
+//  Created by Eliott Ham on 2/11/18.
+//  Copyright © 2018 Eliott Ham. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
